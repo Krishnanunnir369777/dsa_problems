7e09439d95e422a587d1996e69b10a33e1502c6f
Krishnanunni <krishnanunnir503@gmail.com>
2025-02-12 11:49:25 +0530

    some

--- Step_7 Recursion/Lecture 34 Recursion_with_strings/power_of_number.cpp
+++ Step_7 Recursion/Lecture 34 Recursion_with_strings/power_of_number.cpp
@@ -21,6 +21,32 @@ int pow(int a,int b){
     }
 }
 
+
+// O(log(n))
+
+double myPow(double x, int n) {
+        
+    if(n < 0) {
+        x = 1 / x;
+    } 
+    
+    long num = labs(n);
+    
+    double pow = 1;
+    
+    while(num){ // equivalent to while(num != 0)
+        if(num & 1) { // equivalent to if((num & 1) != 0)
+            pow *= x;
+        }
+        
+        x *= x;
+        num >>= 1;
+    }
+    
+    return pow;
+}
+
+
 int main(){
 
     int a,b;
