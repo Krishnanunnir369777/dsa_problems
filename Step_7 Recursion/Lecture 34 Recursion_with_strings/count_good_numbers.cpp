cb5c0b869facbcc741439752b04d1fed092267c8
Krishnanunni <krishnanunnir503@gmail.com>
2025-02-13 20:31:57 +0530

    some

---
+++ Step_7 Recursion/Lecture 34 Recursion_with_strings/count_good_numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+int M=1e9+7;
+long long power(long long b,long long e,long long ans){
+    if(e==0){
+        return ans;
+    }
+    if(e&1){
+        return power(b,e-1,(ans*b)%M);
+    }
+    else{
+        return power((b*b)%M,e/2,ans);
+    }
+
+}
+int func(long long n){
+
+    long long temp=power(20,n/2,1);
+
+    return ( n& 1) ? (temp*5)%M :temp;
+
+}
+
+int main(){
+    long long n=1;
+    cout<<func(n);
+}
