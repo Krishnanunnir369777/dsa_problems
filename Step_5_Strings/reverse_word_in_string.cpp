d2d95f636f6e5cb433deef15c39dc3d402215adc
Krishnanunni <krishnanunnir503@gmail.com>
2025-02-18 15:05:43 +0530

    some changes

---
+++ Step_5_Strings/reverse_word_in_string.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+string reverseWords(string s) {
+    reverse(s.begin(), s.end());
+    int n = s.size();
+    int left = 0;
+    int right = 0;
+    int i = 0;
+    while (i < n) {
+        while (i < n && s[i] == ' ')
+            i++;
+        if (i == n)
+            break;
+        while (i < n && s[i] != ' ') {
+            s[right++] = s[i++];
+        }
+        reverse(s.begin() + left, s.begin() + right);
+        s[right++] = ' ';
+        left = right;
+        i++;
+    }
+    s.resize(right - 1);
+    return s;
+}
+
+int main(){
+    string s="the sky is blue";
+    string ans=func1(s);
+    for(int i=0;i<ans.length();i++){
+        cout<<ans[i];
+    }
+}
