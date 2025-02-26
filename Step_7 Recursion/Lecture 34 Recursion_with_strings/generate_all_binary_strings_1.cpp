0c1920a18ab2244b468724eb8776a88763dd5cb2
Krishnanunni <krishnanunnir503@gmail.com>
2025-02-26 23:04:15 +0530

    some

--- Step_7 Recursion/Lecture 34 Recursion_with_strings/generate_all_binary_strings_1.cpp
+++ Step_7 Recursion/Lecture 34 Recursion_with_strings/generate_all_binary_strings_1.cpp
@@ -2,10 +2,25 @@
 using namespace std;
 #include <bits/stdc++.h>
 
+void generateBinaryStrings(int N, string binaryString = "") {
 
-int main(){
-    int n=3;
-    vector<string> ans=func1(n);
-
+    if (binaryString.length() == N) {
+        for (char c : binaryString) 
+            cout << c << " ";
+        cout << endl;
+        return;
+    }
     
+    generateBinaryStrings(N, binaryString + "0");
+    generateBinaryStrings(N, binaryString + "1");
+}
+
+int main() {
+    int N;
+    cout << "Enter the value of N: ";
+    cin >> N;
+    
+    generateBinaryStrings(N);
+    
+    return 0;
 }
