5f62d0432b8c696aecb58b8de0c58b31ceebf0e9
Krishnanunni <krishnanunnir503@gmail.com>
2025-03-04 20:45:41 +0530

    some

---
+++ Step_7 Recursion/Lecture 34 Recursion_with_strings/combination_sum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+void solve(int index,int target,vector<int>& arr,vector<int>& output,vector<vector<int>>& ans){
+    if(index==arr.size()){
+        if(target==0){
+            ans.push_back(output);
+        }
+        return ;
+    }
+ // pick up the element 
+    if(arr[index]<=target){
+        output.push_back(arr[index]);
+        solve(index,target-arr[index],arr,output,ans);
+        output.pop_back();
+    }
+    solve(index+1,target,arr,output,ans);
+
+}
+vector<vector<int>> func(vector<int>& arr, int target){
+    vector<vector<int>> ans;
+    vector<int> output;
+    solve(0,target,arr,output,ans);
+
+    return ans;
+}
+int main(){
+    vector<int> arr={2,3,6,7};
+    int target=7;
+    vector<vector<int>> ans=func(arr,target);
+}
