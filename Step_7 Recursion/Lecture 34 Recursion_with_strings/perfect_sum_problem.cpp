51cfd2e5fc9521f279e7497af39dde703c09a92f
Krishnanunni <krishnanunnir503@gmail.com>
2025-03-04 14:09:18 +0530

    some

--- Step_7 Recursion/Lecture 34 Recursion_with_strings/perfect_sum_problem.cpp
+++ Step_7 Recursion/Lecture 34 Recursion_with_strings/perfect_sum_problem.cpp
@@ -2,49 +2,70 @@
 using namespace std;
 #include <bits/stdc++.h>
 
+int cnt1=0;
+void solve1(vector<int>&nums,vector<int> output,int index,int target){  //Each element in nums can either be included or excluded, leading to 2 choices per element.
+                                                                      // If the input array has n elements, the recursion will generate 2𝑛^2 nsubsets.
 
-void solve(vector<int> nums,vector<int> output,int index,vector<vector<int>>& ans){
-    //base case
+    //base case                                                   //overall complexity is:O(n*2^n)
     if(index>=nums.size()){
-        ans.push_back(output);
+        int sum=0;
+        for(int i=0;i<output.size();i++){
+            sum+=output[i];
+        }
+        if(sum==target){
+            cnt1++;
+        }
         return ;
     }
 
     //exclude
-    solve(nums,output,index+1,ans);
+    solve1(nums,output,index+1,target);
 
     //include
     int element=nums[index];
     output.push_back(element);
-    solve(nums,output,index+1,ans);
+    solve1(nums,output,index+1,target);
 }
+void func1(vector<int>& nums,int target){
 
-
-vector<vector<int>> subsets(vector<int>& nums){
-
-    vector<vector<int>> ans;
     vector<int> output;
     int index=0;
-    solve(nums,output,index,ans);
-    return ans;
-
+    solve1(nums,output,index,target);
+    
 }
-int main(){
-    vector<int> arr={5, 2, 3, 10, 6, 8};
-    int target=10;
-    int ans=0;
-    vector<vector<int>> ans=subsets(arr);
-    for(auto it:ans;){
-        int sum=0;
-        for(auto i:it;i){
-            sum+=i;
-            if(sum==target){
-                ans++;
 
-            }
-            
+
+//optimised
+
+
+int cnt2=0;
+void solve2(vector<int>& nums, int index, int currentSum, int target) {              //O(2^n)
+    // Base case
+    if (index == nums.size()) {
+        if (currentSum == target) {
+            cnt2++;
         }
+        return;
     }
-    cout<<ans;
+
+    // Exclude the current element
+    solve2(nums, index + 1, currentSum, target);
+
+    // Include the current element
+    solve2(nums, index + 1, currentSum + nums[index], target);
+} 
+void func2(vector<int>& arr,int target){
+    solve2(arr,0,0,target);
+}
+
+
+
+int main(){
+    vector<int> arr={5, 7, 8};
+    int target=3;
+    
+    func2(arr,target);
+    cout<<cnt1;
+    
 
 }
