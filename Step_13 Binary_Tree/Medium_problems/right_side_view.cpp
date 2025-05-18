bf2284b95aab29f14a2b031af2870a0f85a646c9
Krishnanunni <krishnanunnir503@gmail.com>
2025-05-18 17:37:55 +0530

    some

---
+++ Step_13 Binary_Tree/Medium_problems/right_side_view.cpp
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+        }
+        map<int,int>mpp;
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            TreeNode* node=it.first;
+            int level=it.second;
+            if(mpp.find(level)==mpp.end()){
+                mpp[level]=node->val;
+            }
+            
+            if(node->right){
+                q.push({node->right,level+1});
+            }
+            if(node->left){
+                q.push({node->left,level+1});
+            }
+        }
+        for(auto it:mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+        
+        
+    }
