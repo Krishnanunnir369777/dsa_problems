340a3fe384c0888a9bf9109f7910feb109abe729
Krishnanunni <krishnanunnir503@gmail.com>
2025-05-24 20:16:39 +0530

    some

--- random_function/random.cpp
+++ random_function/random.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
+    bool solve(int row,int col,int n,int m,int idx,string word,vector<vector<char>>& board){
+        if(idx==word.length()){
+            return true;
+        }
+        if(row==n || col==m || row<0 || col<0 || board[row][col]!=word[idx] || board[row][col]=='!'){
+            return false;
+        }
+        char c=board[row][col];
+        board[row][col]='!';
 
-        int col0=1;
-        int n=matrix.size();
-        int m=matrix[0].size();
+        bool top=solve(row-1,col,n,m,idx+1,word,board);
+        bool bottom=solve(row+1,col,n,m,idx+1,word,board);
+        bool right=solve(row,col+1,n,m,idx+1,word,board);
+        bool left=solve(row,col-1,n,m,idx+1,word,board);
 
+        board[row][col]=c;
+
+        return top||bottom||right||left;
+        
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n=board.size();
+        int m=board[0].size();
+        int index=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-
-                    if(j!=0){
-                        matrix[0][j]=0;
-                    }
-                    else{
-                        col0=0;
+                if(board[i][j]==word[index]){
+                    if(solve(i,j,n,m,index,word,board)){
+                        return true;
                     }
                 }
             }
         }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][j]!=0 ){
-                    if(matrix[i][0]==0 || matrix[0][j]==0){
-                        matrix[i][j]=0;
-
-                    }
-                }
-            }
-        }
-        if (matrix[0][0] == 0) {
-        for (int j = 0; j < m; j++) {
-            matrix[0][j] = 0;
-        }
-    }
-    if (col0 == 0) {
-        for (int i = 0; i < n; i++) {
-            matrix[i][0] = 0;
-        }
-    }
+        return false;
         
     }
 };
