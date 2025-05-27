27097fecb16e60fb56e43c96177500e80b3790b4
Krishnanunni <krishnanunnir503@gmail.com>
2025-05-27 19:25:29 +0530

    some

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -1,21 +1,39 @@
 
 
 
+
 class Solution {
 public:
-    void func(int curr,int& maxi,TreeNode* root){
-        if(root==NULL){
-            maxi=max(curr,maxi);
-            return ;
-        }
-        func(curr+1,maxi,root->left);
-        func(curr+1,maxi,root->right);
-    }
-    int maxDepth(TreeNode* root) {
-        int maxi=0;
-        int curr=0;
-        func(curr,maxi,root);
+    int majorityElement(vector<int>& arr) {
+        int cnt=0;
+    int ele=0;
+    int n=arr.size();
 
-        return maxi;
+    for(int i=0;i<n;i++){
+        if(cnt==0){
+            cnt=1;
+            ele=arr[i];
+        }
+        else if(arr[i]==ele){
+            cnt++;
+        }
+        else{
+            cnt--;
+        }
+    }   
+    int cnt1=0;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]==ele){
+            cnt1++;
+        }
+    }
+    if(cnt1>(n/2)){
+        return ele;
+    }
+    return -1;
+    
+    
+        
     }
 };
