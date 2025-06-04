33c27a12ea389019002f52ebae5c91f6f1fe82db
Krishnanunni <krishnanunnir503@gmail.com>
2025-06-04 17:51:40 +0530

    some

---
+++ Step_15Graphs/graph_representatiob.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+
+int main()        // space complexity N*N
+{
+    int n, m;
+    cin >> n >> m;
+    // adjacency matrix for undirected graph
+    // time complexity: O(n)
+    int adj[n+1][n+1];
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1  // this statement will be removed in case of directed graph
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+// optimised
+
+int main()          //Space complexity = O(2xE)
+{
+    int n, m;
+    cin >> n >> m;
+    // adjacency list for undirected graph
+    // time complexity: O(2E)
+    vector<int> adj[n+1];
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    return 0;
+}
+
