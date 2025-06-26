019a9d7f555f389610a2167f09057426ec3d8b57
Krishnanunni <krishnanunnir503@gmail.com>
2025-06-26 19:01:27 +0530

    some

--- Lecture 13 Binary_Search_Questions/find_peak_ele_2.cpp
+++ Lecture 13 Binary_Search_Questions/find_peak_ele_2.cpp
@@ -31,6 +31,39 @@
 
 
 
+int findMaxIndex(vector<vector<int>>& mat,int n,int col){
+    int max_val=-1;
+    int max_idx=-1;
+    for(int i=0;i<n;i++){
+        if(mat[i][col]>max_val){
+            max_idx=i;
+            max_val=mat[i][col];
+        }
+    }
+    return max_idx;
+   }
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {  //O(n*log(m))
+        int n=mat.size();
+        int m=mat[0].size();
+        int s=0;
+        int e=m-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            int max_row_idx=findMaxIndex(mat,n,mid);
+            int left=mid-1>=0 ? mat[max_row_idx][mid-1] : -1;
+            int right=mid+1<m ? mat[max_row_idx][mid+1] : -1;
+            if(mat[max_row_idx][mid]>left && mat[max_row_idx][mid]>right){
+                return {max_row_idx,mid};
+            }
+            else if(mat[max_row_idx][mid]<left){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
 
+        }
+        return {-1,-1};
+    }
 
     
