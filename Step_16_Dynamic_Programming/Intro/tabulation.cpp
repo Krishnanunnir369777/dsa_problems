8aa7ea68d84b9c9b40e5a92990ccdf4780752000
Krishnanunni <krishnanunnir503@gmail.com>
2025-07-04 23:31:23 +0530

    some

---
+++ Step_16_Dynamic_Programming/Intro/tabulation.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main() {
+
+  int n=5;
+  vector<int> dp(n+1,-1);
+  
+  dp[0]= 0;
+  dp[1]= 1;
+  
+  for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+}
+}
+
+
+//optimised
+
+int main(){
+    int n=5;
+    int prev2=0;
+    int prev1=1;
+
+    for(int i=2;i<=n;i++){
+        int curr=prev1+prev2;
+        prev2=prev1;
+        prev1=curr;
+    }
+    cout<<prev1;
+
+    return 0;
+}
