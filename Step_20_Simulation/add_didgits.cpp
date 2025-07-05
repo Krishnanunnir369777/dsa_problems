ee042ab7a172186d3a2c41936c3c6a9d01376881
Krishnanunni <krishnanunnir503@gmail.com>
2025-07-05 21:49:07 +0530

    some

---
+++ Step_20_Simulation/add_didgits.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum=0;
+        while(num){
+            sum+=num%10;
+            num=num/10;
+        }
+        if(sum<10){
+            return sum;
+        }
+        return addDigits(sum);
+    }
+};
+
+
+
+
+
+class Solution {
+public:
+    int addDigits(int num) {
+      int sum=0;
+        while(num>9)
+        {
+            while(num)
+            {
+                sum+=(num%10);
+                num/=10;
+            }
+            num=sum;
+            sum=0;
+        }
+        return num;
+    }
+};
--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-
+ 
+class Node{
+    public:
+    int data;
+    Node* next;
+    public:
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+Node* insertHeaad(Node* head,int val){
+    Node* temp=new Node(val);
+    temp->next=head;
+    return temp;
+}
 int main(){
-    int n,m;
+    vector<int> arr = {12, 8, 5, 7};
+    int val = 100;
+
+    // Creating a linked list with initial elements from the array
+    Node* head = new Node(arr[0]);
+    head->next = new Node(arr[1]);
+    head->next->next = new Node(arr[2]);
+    head->next->next->next = new Node(arr[3]);
+
+    // Inserting a new node at the head of the linked list
+    head = insertHead(head, val);
+    // Printing the linked list
+    printLL(head);
+
+    return 0;
     
 }
