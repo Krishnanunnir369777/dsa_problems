8ff92586f30457b75cae476cc4fe319c74affceb
Krishnanunni <krishnanunnir503@gmail.com>
2025-07-18 01:45:15 +0530

    some

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -2,7 +2,16 @@
 #include <bits/stdc++.h>
 using namespace std;
  
+int func(int n,int r){
+    int ans=1;
+    for(int i=0;i<r;i++){
+        ans=ans*(n-i);
+        ans=ans/(i+1);
+    }
+    return ans;
+
+}
 int main(){
-    cout<<6%2;
+    
     
 }
