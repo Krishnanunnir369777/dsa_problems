88955e7ce501656367c9d82312200f33dd816f9a
Krishnanunni <krishnanunnir503@gmail.com>
2025-07-22 21:30:01 +0530

    some

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -3,15 +3,3 @@
 using namespace std;
  
 
-int main() {
-    vector<vector<int>> matrix{
-        {5, 9, 6},
-        {11, 5, 2}
-    };
-
-    int n = matrix.size();
-    int m = matrix[0].size();
-
-    cout << "Minimum sum path: " << minSumPath(n, m, matrix) << endl;
-    return 0;
-}
