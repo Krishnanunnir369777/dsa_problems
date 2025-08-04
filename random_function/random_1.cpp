0855240d4faee1a201eda1ff89f9d795aebabf7a
Krishnanunni <krishnanunnir503@gmail.com>
2025-08-04 22:52:33 +0530

    some

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -5,12 +5,22 @@ using namespace std;
 
 int func(vector<int>& nums){
     int n=nums.size();
-    
+    if(n==0 || n==1){
+        return nums;
+    }
+    vector<vector<int>> ans;
+    for(int i=1;i<n;i++){
+        vector<int>& back=ans.back();
+        if(nums[i])
+
+    }
     
 }
 
 
 int main(){
+
+
     
     
 }
