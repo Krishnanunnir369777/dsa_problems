ff6805342442e9cbd32946f3a87f05a242f08e9c
Krishnanunni <krishnanunnir503@gmail.com>
2025-08-19 00:52:38 +0530

    some

---
+++ Step_16_Dynamic_Programming/dp_strings/minimum_insertion_to_convert_string_a_to_b.cpp
@@ -0,0 +1,64 @@
+
+
+//memoization
+
+class Solution {
+public:
+    int func(int i,int j,string& s1,string& s2,vector<vector<int>>& dp){  //O(n*m)
+        if(i<0 || j<0){ 
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1[i]==s2[j]){
+            return dp[i][j]=1+func(i-1,j-1,s1,s2,dp);
+        }
+        else{
+            return dp[i][j]=max(func(i-1,j,s1,s2,dp),func(i,j-1,s1,s2,dp));
+        }
+    }
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        int lcs=func(n-1,m-1,word1,word2,dp);
+
+        return n+m-2*lcs;
+    }
+};
+
+
+
+
+
+//tabulation
+
+
+class Solution {
+public:
+    
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return n+m-2*dp[n][m];
+    }
+};
+
+
+
+
+//optimised
+
+
