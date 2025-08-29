43e6bdc7b386cbce916e6a3fe176055ca904a90b
Krishnanunni <krishnanunnir503@gmail.com>
2025-08-30 01:52:27 +0530

    some

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -1,17 +1,45 @@
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
- 
+class Solution {
+public:
+   bool dfs(int node,vector<int>& vis,vector<int>& pathvis,vector<int>& check,vector<vector<int>>& graph){
+    vis[node]=1;
+    pathvis[node]=1;
+    check[node]=0;
 
-
-vector<int> dfs_graph(int V,vector<int> adj[]){
-    int vis[V]={0};
-    vis[0]=1;
-    dfs()
-}
-
-int main(){
-      
-    
-}
-
+    for(auto it:graph[node]){
+        if(!vis[it]){
+            if(dfs(it,vis,pathvis,check,graph)==true){
+                vis[it]=1;
+                check[it]=0;
+                return true;
+            }
+        }
+        else if(pathvis[it]){
+            vis[it]=1;
+            check[it]=0;
+            return true;
+        }
+    }
+    check[node]=1;
+    pathvis[node]=0;
+    return false;
+   }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> vis(n,0);
+        vector<int> pathvis(n,0);
+        vector<int> check(n,0);
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,vis,pathvis,check,graph);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(check[i]==1){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+        
+    }
+};
