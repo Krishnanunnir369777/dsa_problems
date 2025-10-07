0876a8a3b50c45b0623dd32f9ca843c1fe825974
Krishnanunnir369777 <krishnanunnir503@gmail.com>
2025-10-07 23:11:58 +0530

    some

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -1,29 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool canPartition(int n,vector<int>& arr){
-    int sum=0;
-    for(int i=0;i<n;i++){
-        sum+=arr[i];
+
+int knapsackUtil(vector<int>& wt,vector<int>& val,int idx,int W,vector<vector<int>>& dp){
+    if(idx==0){
+        return (W/wt[0])*val[0];
     }
-    if(sum%2!=0){
-        return false;
+    if(dp[idx][W]!=-1){
+        return dp[idx][W];
     }
-    else{
-        int target=sum/2;
-        vector<vector<bool>> dp(n,vector<bool>(target+1,false));
-        
-        
+    int not_take=knapsackUtil(wt,val,idx-1,W,dp);
+    int take=INT_MIN;
+    if(wt[idx]<=W){
+        take=val[idx]+knapsackUtil(wt,val,idx,W-wt[idx],dp);
     }
+    return dp[idx][W]=max(not_take,take);
+
 }
+
+int unboundedKnapsack(int n, int W, vector<int>& val, vector<int>& wt) {
+    vector<vector<int>> dp(n, vector<int>(W + 1, -1)); // Create a DP table
+    
+    // Call the utility function to calculate the maximum value
+    return knapsackUtil(wt, val, n - 1, W, dp);
+}
+
 int main() {
-    vector<int> arr = {2, 3, 3, 3, 4, 5};
-    int n = arr.size();
+    vector<int> wt = {2, 4, 6}; // Weight of items
+    vector<int> val = {5, 11, 13}; // Value of items
+    int W = 10; // Weight capacity of the knapsack
+    int n = wt.size(); // Number of items
+    
+    // Call the function to calculate and output the maximum value the thief can steal
+    cout << "The Maximum value of items the thief can steal is " << unboundedKnapsack(n, W, val, wt) << endl;
 
-    if (canPartition(n, arr))
-        cout << "The Array can be partitioned into two equal subsets";
-    else
-        cout << "The Array cannot be partitioned into two equal subsets";
-
-    return 0;
+    return 0; // Return 0 to indicate successful program execution
 }
