97b35a0e6a2a7ce380f6c0b9ae75c08e707b7bfd
Krishnanunnir369777 <krishnanunnir503@gmail.com>
2025-10-27 21:55:24 +0530

    some

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -1,38 +1,50 @@
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution{
+    public:
+        bool dfs(int node,int parent,vector<int>& vis,vector<int> adj[]){
+            vis[node]=1;
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    dfs(it,node,vis,adj);
+                }
+                else if(it)
+            }
+        }
+        bool isCycle(int V,vector<int> adj[]){
+            vector<int> vis(V,0);
+            for(int i=0;i<V;i++){
+                if(!vis[i]){
+                    if(dfs(i,-1,vis,adj)){
+                    return true;
+                }
+                }
+            }
+            return false;
+        }
 
-int knapsackUtil(vector<int>& wt,vector<int>& val,int idx,int W,vector<vector<int>>& dp){
-    if(idx==0){
-        return (W/wt[0])*val[0];
-    }
-    if(dp[idx][W]!=-1){
-        return dp[idx][W];
-    }
-    int not_take=knapsackUtil(wt,val,idx-1,W,dp);
-    int take=INT_MIN;
-    if(wt[idx]<=W){
-        take=val[idx]+knapsackUtil(wt,val,idx,W-wt[idx],dp);
-    }
-    return dp[idx][W]=max(not_take,take);
+};
 
-}
+int main(){
+    int V=5;
+    vector<int> adj[V];
+    adj[0].push_back(1);
+    adj[1].push_back(0);
+    adj[1].push_back(2);
+    adj[2].push_back(1);
+    adj[2].push_back(3);
+    adj[3].push_back(2);
+    adj[3].push_back(4);
+    adj[4].push_back(3);
+    adj[4].push_back(1); 
 
-int unboundedKnapsack(int n, int W, vector<int>& val, vector<int>& wt) {
-    vector<vector<int>> dp(n, vector<int>(W + 1, -1)); // Create a DP table
-    
-    // Call the utility function to calculate the maximum value
-    return knapsackUtil(wt, val, n - 1, W, dp);
-}
+    Solution sol;
+    if (sol.isCycle(V, adj))
+        cout << "Cycle detected\n";
+    else
+        cout << "No cycle found\n";
 
-int main() {
-    vector<int> wt = {2, 4, 6}; // Weight of items
-    vector<int> val = {5, 11, 13}; // Value of items
-    int W = 10; // Weight capacity of the knapsack
-    int n = wt.size(); // Number of items
-    
-    // Call the function to calculate and output the maximum value the thief can steal
-    cout << "The Maximum value of items the thief can steal is " << unboundedKnapsack(n, W, val, wt) << endl;
-
-    return 0; // Return 0 to indicate successful program execution
+    return 0;
 }
