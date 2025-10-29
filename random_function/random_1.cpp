37a86d0bbbd28367a254de6a8e14caec5e1d8679
Krishnanunnir369777 <krishnanunnir503@gmail.com>
2025-10-29 22:54:11 +0530

    some

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -2,49 +2,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public:
-        bool dfs(int node,int parent,vector<int>& vis,vector<int> adj[]){
-            vis[node]=1;
-            for(auto it:adj[node]){
-                if(!vis[it]){
-                    dfs(it,node,vis,adj);
-                }
-                else if(it)
+class Solution {
+private:
+	
+	bool dfsCheck(int node,vector<int> adj[],int vis[].int pathVis[]){
+        vis[node]=1;
+        pathVis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[node]){
+                dfs(it,adj)
             }
         }
-        bool isCycle(int V,vector<int> adj[]){
-            vector<int> vis(V,0);
-            for(int i=0;i<V;i++){
-                if(!vis[i]){
-                    if(dfs(i,-1,vis,adj)){
-                    return true;
-                }
-                }
-            }
-            return false;
-        }
+    }
+public:
+	// Function to detect cycle in a directed graph.
+	bool isCyclic(int V, vector<int> adj[]) {
+		int vis[V] = {0};
+		int pathVis[V] = {0};
 
+		for (int i = 0; i < V; i++) {
+			if (!vis[i]) {
+				if (dfsCheck(i, adj, vis, pathVis) == true) return true;
+			}
+		}
+		return false;
+	}
 };
 
-int main(){
-    int V=5;
-    vector<int> adj[V];
-    adj[0].push_back(1);
-    adj[1].push_back(0);
-    adj[1].push_back(2);
-    adj[2].push_back(1);
-    adj[2].push_back(3);
-    adj[3].push_back(2);
-    adj[3].push_back(4);
-    adj[4].push_back(3);
-    adj[4].push_back(1); 
 
-    Solution sol;
-    if (sol.isCycle(V, adj))
-        cout << "Cycle detected\n";
-    else
-        cout << "No cycle found\n";
+int main() {
 
-    return 0;
+	// V = 11, E = 11;
+	vector<int> adj[11] = {{}, {2}, {3}, {4, 7}, {5}, {6}, {}, {5}, {9}, {10}, {8}};
+	int V = 11;
+	Solution obj;
+	bool ans = obj.isCyclic(V, adj);
+
+	if (ans)
+		cout << "True\n";
+	else
+		cout << "False\n";
+
+	return 0;
 }
