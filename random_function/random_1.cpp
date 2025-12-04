91fd54f1dfc110a53a203184c07811e59f0c7d70
Krishnanunnir369777 <krishnanunnir503@gmail.com>
2025-12-04 22:27:06 +0530

    some

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -1,52 +1,58 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-class Solution{
+class DisjointSet{
+    vector<int> rank,parent;
     public:
-    vector<int> dijkstra(int V,vector<vector<pair<int,int>>>& adj,int src){
-        vector<int> dist(V,1e9);
-        dist[src]=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,src});
-        while(!pq.empty()){
-            int node=pq.top().second;
-            int dis=pq.top().first;
-            pq.pop();
-            if(dis>dist[node]){
-                continue;
-            }
-            for(auto it:adj[node]){
-                int next=it.first;
-                int wt=it.second;
-                if(dist[node]+wt<dist[next]){
-                    dist[next]=dist[node]+wt;
-                    pq.push({dist[next],next});
-                }
-            }
+    DisjointSet(int n){
+        rank(n+1,0);
+        parent(n+1,0);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
         }
-        return dist;
+    }
+    int findUPar(int node){
+        if(parent[node]==node){
+            return node;
+        }
+        return parent[node]=findUPar(parent[node]);
+    }
+    void unionByRank(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v){
+            return ;
+        }
+        if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u]=ulp_vl;
+        }
+        else if(rank[ulp_u]>rank[ulp_v]){
+            parent[ulp_v]=ulp_u;
+        }
+        else{
+            parent[ulp_u]=ulp_v;
+            rank[ulp_u]++;
+        }
 
     }
 };
-
-int main(){
-    int V = 5;
-
-    // Adjacency list {neighbor, weight}
-    vector<vector<pair<int,int>>> adj(V);
-
-    // Example edges
-    adj[0].push_back({1, 2});
-    adj[0].push_back({2, 4});
-    adj[1].push_back({2, 1});
-    adj[1].push_back({3, 7});
-    adj[2].push_back({4, 3});
-    adj[3].push_back({4, 2});
-
-    Solution obj;
-    vector<int> ans=obj.dijkstra(V,adj,0);
-    for(int i=0;i<ans.size();i++){
-        cout<<ans[i]<<" ";
+int main() {
+    DisjointSet ds(7);
+    ds.unionByRank(1, 2);
+    ds.unionByRank(2, 3);
+    ds.unionByRank(4, 5);
+    ds.unionByRank(6, 7);
+    ds.unionByRank(5, 6);
+    // if 3 and 7 same or not
+    if (ds.findUPar(3) == ds.findUPar(7)) {
+        cout << "Same\n";
     }
+    else cout << "Not same\n";
+
+    ds.unionByRank(3, 7);
+
+    if (ds.findUPar(3) == ds.findUPar(7)) {
+        cout << "Same\n";
+    }
+    else cout << "Not same\n";
+    return 0;
 }
