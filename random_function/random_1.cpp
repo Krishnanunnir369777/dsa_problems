a642f89e63addee211387411a81635da9263627f
Krishnanunnir369777 <krishnanunnir503@gmail.com>
2025-12-05 23:28:32 +0530

    some

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -1,58 +1,102 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+
 class DisjointSet{
-    vector<int> rank,parent;
+    vector<int> parent,size;
     public:
     DisjointSet(int n){
-        rank(n+1,0);
-        parent(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1,1);
         for(int i=0;i<=n;i++){
             parent[i]=i;
         }
     }
-    int findUPar(int node){
+    int ultp(int node){
         if(parent[node]==node){
             return node;
         }
-        return parent[node]=findUPar(parent[node]);
+        return parent[node]=ultp(parent[node]);
     }
-    void unionByRank(int u,int v){
-        int ulp_u=findUPar(u);
-        int ulp_v=findUPar(v);
-        if(ulp_u==ulp_v){
+    void JoinBySize(int u,int v){
+        int ult_u=ultp(u);
+        int ult_v=ultp(v);
+        if(ult_u==ult_v){
             return ;
         }
-        if(rank[ulp_u]<rank[ulp_v]){
-            parent[ulp_u]=ulp_vl;
-        }
-        else if(rank[ulp_u]>rank[ulp_v]){
-            parent[ulp_v]=ulp_u;
+        if(size[ult_v]>size[ult_u]){
+            parent[ult_u]=ult_v;
+            size[ult_v]+=size[ult_u];
         }
         else{
-            parent[ulp_u]=ulp_v;
-            rank[ulp_u]++;
+            parent[ult_v]=ult_u;
+            size[ult_u]+=size[ult_v];
         }
+    }
+};
+// Solution class
+class Solution{
+public:
+
+    // Function to get the sum of weights of edges in MST
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        
+        // To store the edges
+        vector<pair<int,pair<int,int>>> edges;
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                int v=it[0];
+                int wt=it[1];
+                edges.push_back({wt,{i,v}});
+            }
+        }
+        DisjointSet ds(V);
+        sort(edges.begin(),edges.end());
+        int sum=0;
+        for(auto it:edges){
+            int wt=it.first;
+            int v=it.second.first;
+            int u=it.second.second;
+            if(ds.ultp(v)!=ds.ultp(u)){
+                sum+=wt;
+                ds.JoinBySize(u,v);
+            }
+
+        }
+        return sum;
 
     }
 };
+
+
 int main() {
-    DisjointSet ds(7);
-    ds.unionByRank(1, 2);
-    ds.unionByRank(2, 3);
-    ds.unionByRank(4, 5);
-    ds.unionByRank(6, 7);
-    ds.unionByRank(5, 6);
-    // if 3 and 7 same or not
-    if (ds.findUPar(3) == ds.findUPar(7)) {
-        cout << "Same\n";
+    int V = 4;
+    vector<vector<int>> edges = {
+        {0, 1, 1},
+        {1, 2, 2},
+        {2, 3, 3},
+        {0, 3, 4}
+    };
+    
+    // Forming the adjacency list from edges
+    vector<vector<int>> adj[4];
+    for(auto it : edges) {
+        int u = it[0];
+        int v = it[1];
+        int wt = it[2];
+        
+        adj[u].push_back({v, wt});
+        adj[v].push_back({u, wt});
     }
-    else cout << "Not same\n";
-
-    ds.unionByRank(3, 7);
-
-    if (ds.findUPar(3) == ds.findUPar(7)) {
-        cout << "Same\n";
-    }
-    else cout << "Not same\n";
+    
+    // Creating instance of Solution class
+    Solution sol;
+    
+    /* Function call to get the sum 
+    of weights of edges in MST */
+    int ans = sol.spanningTree(V, adj);
+    
+    cout << "The sum of weights of edges in MST is: " << ans;
+    
     return 0;
 }
