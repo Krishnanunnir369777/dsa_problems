2ed451b96dd532d5ae9fdc0c35b4349942a3058b
Krishnanunnir369777 <krishnanunnir503@gmail.com>
2025-12-21 21:53:22 +0530

    some

--- Step_8_bit_manupulation/Lec_1/prime_factors.cpp
+++ Step_8_bit_manupulation/Lec_1/prime_factors.cpp
@@ -30,9 +30,48 @@ void primeFactors(int n)
 
 /* Driver program to test above function */
 
+// Driver code
+int main() {  //easier code
+    int n=17;
+    vector<int> ans;
+    
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            ans.push_back(i);
+            while(n%i==0){
+                n=n/i;
+            }
+        }
+    }
+    if(n!=1){
+        ans.push_back(n);
+    }
+    for(int i:ans){
+        cout<<i<<" ";
+    }
+    
+}
+
+
+
+
+
+
 int main() 
 { 
     int n = 18; 
     primeFactors(n); 
     return 0; 
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -5,8 +5,22 @@ using namespace std;
 
 // Driver code
 int main() {
-    int a,b;
-    a=a^b;
-    b=a^b;
-    a=a^b;
+    int n=17;
+    vector<int> ans;
+    
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            ans.push_back(i);
+            while(n%i==0){
+                n=n/i;
+            }
+        }
+    }
+    if(n!=1){
+        ans.push_back(n);
+    }
+    for(int i:ans){
+        cout<<i<<" ";
+    }
+    
 }
