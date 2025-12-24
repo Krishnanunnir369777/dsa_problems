cf8075be359604c0150096b48793ff83edcafa9a
Krishnanunnir369777 <krishnanunnir503@gmail.com>
2025-12-24 21:35:06 +0530

    some

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -5,22 +5,10 @@ using namespace std;
 
 // Driver code
 int main() {
-    int n=17;
-    vector<int> ans;
-    
-    for(int i=2;i*i<=n;i++){
-        if(n%i==0){
-            ans.push_back(i);
-            while(n%i==0){
-                n=n/i;
-            }
-        }
-    }
-    if(n!=1){
-        ans.push_back(n);
-    }
-    for(int i:ans){
-        cout<<i<<" ";
-    }
+    int n=1;
+    int *ptr=&n;
+    int **ptr2=&ptr;
+    cout<<&ptr<<endl;
+    cout<<ptr2;
     
 }
